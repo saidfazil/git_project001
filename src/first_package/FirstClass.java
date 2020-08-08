f71a4fa3d21c597a763651039dc10f562886b5ad
Said <said.fserik@gmail.com>
2020-08-08 13:46:27 -0400

    My Local Settar Branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
-
+		System.out.println("Our second commit");
+		System.out.println("now i am on my settar branch");
 	}
 }
